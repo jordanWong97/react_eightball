aabd7a382c5b3faf8f3be08529dd3f393476e7f2
Emily Chen <sticky_sugar@hotmail.com>
2022-08-30 11:57:59 -0700

    change color and answers

---
+++ src/Eightball.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const EIGHTBALL_ANSWERS = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+];
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+function Eightball({ answers=EIGHTBALL_ANSWERS }) {
+
+  const [answer, setAnswer] = useState({
+    msg: "think of a question",
+    color: "black"
+  });
+
+  function changeAns(){
+    const randomAns = shuffle(answers);
+    setAnswer(randomAns[0]);
+  }
+
+  return (
+    <div
+    style={{
+      backgroundColor:'black',
+      width: '200px',
+      height: '200px',
+      borderRadius: '50%',
+      color: 'white'
+    }}>
+
+      <h3 onClick={changeAns}>{answer.msg}</h3>
+    </div>
+  )
+}
+
+export default Eightball;
